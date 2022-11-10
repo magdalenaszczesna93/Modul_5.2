3c6202d859f94a02618d8f1a40fcafa5e2ad7648
Magdalena Szczesna <magdalena.szczesna.ue@gmil.com>
2022-11-10 17:30:35 +0100

    add change dekorator

--- dekorator.py
+++ dekorator.py
@@ -1,4 +1,5 @@
-
+from faker import Faker
+fake = Faker()
 
 def say_louder(func):
     def wrapper():
@@ -13,3 +14,14 @@ def say_hello():
     
 print(say_hello())
 
+def card():
+    list=[]
+    for i in range(1000):
+        list.append(name = fake.name(
+            first_name = fake.first_name, 
+            last_name = fake.last_name, 
+            phone_number = fake.phone_number, 
+            email = fake.email))
+    return list
+
+print(card())
