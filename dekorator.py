9c8736ced558c0153f3b9fd58c071cde4667918c
Magdalena Szczesna <magdalena.szczesna.ue@gmil.com>
2022-11-15 16:14:07 +0100

    chages

--- dekorator.py
+++ dekorator.py
@@ -1,9 +1,6 @@
 from faker import Faker
 fake = Faker()
 
-from datetime import time
-t=time()
-
 def say_louder(func):
     def wrapper():
         result = func()
@@ -16,22 +13,3 @@ def say_hello():
     return greeting
     
 print(say_hello())
-
-class Contact:
-    def __init__(self, first_name, last_name, phone_number, email):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.phone_number = phone_number
-        self.email = email
-
-
-def card():
-        list=[]
-        for i in range(1000):
-            list.append(Contact(
-                first_name = fake.first_name(), 
-                last_name = fake.last_name(), 
-                phone_number = fake.phone_number(), 
-                email = fake.email()))
-        return list
-print(card())
