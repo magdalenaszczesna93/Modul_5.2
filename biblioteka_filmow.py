255aa48b8f68a476b2a6b192bde9722feec55a6d
Magdalena Szczesna <magdalena.szczesna.ue@gmil.com>
2022-11-16 17:16:07 +0100

    add soted lists

--- biblioteka_filmow.py
+++ biblioteka_filmow.py
@@ -1,4 +1,4 @@
-class filmy:
+class film:
     def __init__ (self, tytul, rok_wydania, gatunek, liczba_odtworzen):
         self.tytul = tytul
         self.rok_wydania = rok_wydania
@@ -14,12 +14,12 @@ def play (self):
         self.liczba_odtworzen += 1
         return f'{self.tytul} odtworzono {self.liczba_odtworzen} razy.'
 
-movie1 = filmy(tytul='Dogs', rok_wydania='1994', gatunek='komedia', liczba_odtworzen=20)
-movie2 = filmy(tytul='Cats', rok_wydania='1995', gatunek='dokumentalny', liczba_odtworzen=10)
-movie3 = filmy(tytul='Tiger', rok_wydania='1998', gatunek='biograficzny', liczba_odtworzen=25)
-movie4 = filmy(tytul='Home', rok_wydania='2005', gatunek='horror', liczba_odtworzen=35)
+movie1 = film(tytul='Dogs', rok_wydania='1994', gatunek='komedia', liczba_odtworzen=20)
+movie2 = film(tytul='Cats', rok_wydania='1995', gatunek='dokumentalny', liczba_odtworzen=10)
+movie3 = film(tytul='Tiger', rok_wydania='1998', gatunek='biograficzny', liczba_odtworzen=25)
+movie4 = film(tytul='Home', rok_wydania='2005', gatunek='horror', liczba_odtworzen=35)
 
-class seriale(filmy):
+class serial(film):
     def __init__(self, numer_odcinka, numer_sezonu, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.numer_odcinka = numer_odcinka
@@ -34,10 +34,10 @@ def play (self):
         self.liczba_odtworzen += 1
         return f'{self.tytul} odtworzono {self.liczba_odtworzen} razy.'
 
-series1 = seriale(tytul='The Simpsons', rok_wydania='1994', gatunek='komedia', liczba_odtworzen=20, numer_odcinka= 1, numer_sezonu= 1)
-series2 = seriale(tytul='Game of Thrones', rok_wydania='2000', gatunek='obyczajowy', liczba_odtworzen=45, numer_odcinka= 1, numer_sezonu= 1)
-series3 = seriale(tytul='Friends', rok_wydania='1995', gatunek='komedia', liczba_odtworzen=35, numer_odcinka= 1, numer_sezonu= 1)
-series4 = seriale(tytul='Star Trek', rok_wydania='2004', gatunek='horror', liczba_odtworzen=10, numer_odcinka= 1, numer_sezonu= 1)
+series1 = serial(tytul='The Simpsons', rok_wydania='1994', gatunek='komedia', liczba_odtworzen=20, numer_odcinka= 1, numer_sezonu= 1)
+series2 = serial(tytul='Game of Thrones', rok_wydania='2000', gatunek='obyczajowy', liczba_odtworzen=45, numer_odcinka= 1, numer_sezonu= 1)
+series3 = serial(tytul='Friends', rok_wydania='1995', gatunek='komedia', liczba_odtworzen=35, numer_odcinka= 1, numer_sezonu= 1)
+series4 = serial(tytul='Star Trek', rok_wydania='2004', gatunek='horror', liczba_odtworzen=10, numer_odcinka= 1, numer_sezonu= 1)
 
 print(play(movie1))
 print(movie1)
@@ -59,3 +59,22 @@ lista_all.append(series2)
 lista_all.append(series3)
 lista_all.append(series4)
 print(lista_all)
+
+def get_movies():
+    movies = []
+    for i in lista_all:
+        if type(i) == film:
+            movies.append(i)
+            sorted_movies = sorted(movies, key=lambda movie: movie.tytul)
+    return sorted_movies
+
+def get_series():
+    series = []
+    for i in lista_all:
+        if type(i) == serial:
+            series.append(i)
+            sorted_series = sorted(series, key=lambda series1: series1.tytul)
+    return sorted_series
+
+print(get_movies())
+print(get_series())
