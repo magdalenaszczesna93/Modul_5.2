aff88de5d931edcc5fcc6f8800b1f23e92c81b4f
Magdalena Szczesna <magdalena.szczesna.ue@gmil.com>
2022-11-16 16:54:19 +0100

    add list_all

--- biblioteka_filmow.py
+++ biblioteka_filmow.py
@@ -6,6 +6,8 @@ class filmy:
         self.liczba_odtworzen = liczba_odtworzen
     def __str__ (self):
         return f'{self.tytul} ({self.rok_wydania})'
+    def __repr__ (self):
+        return f'{self.tytul} ({self.rok_wydania}) {self.gatunek} {self.liczba_odtworzen}'
 
 #jak zorbić żeby to zwięskzało na stałę listę odtworzeń i za każdym razem dodawało?
 def play (self):
@@ -23,7 +25,10 @@ class seriale(filmy):
         self.numer_odcinka = numer_odcinka
         self.numer_sezonu = numer_sezonu
     def __str__ (self):
-        return f'{self.tytul} {self.rok_wydania} {self.gatunek} {self.liczba_odtworzen} S{self.numer_odcinka:02d}E{self.numer_sezonu:02d}'
+        return f'{self.tytul} S{self.numer_odcinka:02d}E{self.numer_sezonu:02d}'
+    def __repr__ (self):
+        return f'{self.tytul} S{self.numer_odcinka:02d}E{self.numer_sezonu:02d} ({self.rok_wydania}) {self.gatunek} {self.liczba_odtworzen}'
+    
 
 def play (self):
         self.liczba_odtworzen += 1
@@ -34,8 +39,6 @@ series2 = seriale(tytul='Game of Thrones', rok_wydania='2000', gatunek='obyczajo
 series3 = seriale(tytul='Friends', rok_wydania='1995', gatunek='komedia', liczba_odtworzen=35, numer_odcinka= 1, numer_sezonu= 1)
 series4 = seriale(tytul='Star Trek', rok_wydania='2004', gatunek='horror', liczba_odtworzen=10, numer_odcinka= 1, numer_sezonu= 1)
 
-lista_all = []
-
 print(play(movie1))
 print(movie1)
 print(play(movie1))
@@ -45,3 +48,14 @@ print(series2)
 print(play(series2))
 print(series2)
 print(series3)
+
+lista_all = []
+lista_all.append(movie1)
+lista_all.append(movie2)
+lista_all.append(movie3)
+lista_all.append(movie4)
+lista_all.append(series1)
+lista_all.append(series2)
+lista_all.append(series3)
+lista_all.append(series4)
+print(lista_all)
