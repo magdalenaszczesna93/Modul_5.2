aed05d5a26f68bb036639dd4b60db464cd1dcb8d
Magdalena Szczesna <magdalena.szczesna.ue@gmil.com>
2022-11-16 19:45:30 +0100

    add searcher by title

--- biblioteka_filmow.py
+++ biblioteka_filmow.py
@@ -1,51 +1,68 @@
-class film:
-    def __init__ (self, tytul, rok_wydania, gatunek, liczba_odtworzen):
+class Film:
+    def __init__ (self, tytul, rok_wydania, gatunek):
         self.tytul = tytul
         self.rok_wydania = rok_wydania
         self.gatunek = gatunek
-        self.liczba_odtworzen = liczba_odtworzen
+        self.liczba_odtworzen = 0
+
     def __str__ (self):
         return f'{self.tytul} ({self.rok_wydania})'
+
     def __repr__ (self):
         return f'{self.tytul} ({self.rok_wydania}) {self.gatunek} {self.liczba_odtworzen}'
 
-#jak zorbić żeby to zwięskzało na stałę listę odtworzeń i za każdym razem dodawało?
-def play (self):
+    def play (self):
         self.liczba_odtworzen += 1
-        return f'{self.tytul} odtworzono {self.liczba_odtworzen} razy.'
-
-movie1 = film(tytul='Dogs', rok_wydania='1994', gatunek='komedia', liczba_odtworzen=20)
-movie2 = film(tytul='Cats', rok_wydania='1995', gatunek='dokumentalny', liczba_odtworzen=10)
-movie3 = film(tytul='Tiger', rok_wydania='1998', gatunek='biograficzny', liczba_odtworzen=25)
-movie4 = film(tytul='Home', rok_wydania='2005', gatunek='horror', liczba_odtworzen=35)
-
-class serial(film):
+            
+class Serial(Film):
     def __init__(self, numer_odcinka, numer_sezonu, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.numer_odcinka = numer_odcinka
         self.numer_sezonu = numer_sezonu
+
     def __str__ (self):
         return f'{self.tytul} S{self.numer_odcinka:02d}E{self.numer_sezonu:02d}'
+
     def __repr__ (self):
         return f'{self.tytul} S{self.numer_odcinka:02d}E{self.numer_sezonu:02d} ({self.rok_wydania}) {self.gatunek} {self.liczba_odtworzen}'
-    
 
-def play (self):
-        self.liczba_odtworzen += 1
-        return f'{self.tytul} odtworzono {self.liczba_odtworzen} razy.'
+def get_movies(lista_all):
+    movies = []
+    for i in lista_all:
+        if type(i) == Film:
+            movies.append(i)
+            sorted_movies = sorted(movies, key=lambda movie: movie.tytul)
+    return sorted_movies
 
-series1 = serial(tytul='The Simpsons', rok_wydania='1994', gatunek='komedia', liczba_odtworzen=20, numer_odcinka= 1, numer_sezonu= 1)
-series2 = serial(tytul='Game of Thrones', rok_wydania='2000', gatunek='obyczajowy', liczba_odtworzen=45, numer_odcinka= 1, numer_sezonu= 1)
-series3 = serial(tytul='Friends', rok_wydania='1995', gatunek='komedia', liczba_odtworzen=35, numer_odcinka= 1, numer_sezonu= 1)
-series4 = serial(tytul='Star Trek', rok_wydania='2004', gatunek='horror', liczba_odtworzen=10, numer_odcinka= 1, numer_sezonu= 1)
+def get_series(lista_all):
+    series = []
+    for i in lista_all:
+        if type(i) == Serial:
+            series.append(i)
+            sorted_series = sorted(series, key=lambda series1: series1.tytul)
+    return sorted_series
 
-print(play(movie1))
-print(movie1)
-print(play(movie1))
-print(movie1)
-print(play(series2))
-print(series2)
-print(play(series2))
+def search(title):
+    for i in lista_all:
+        if title == i.tytul:
+            return i
+
+movie1 = Film(tytul='Dogs', rok_wydania='1994', gatunek='komedia')
+movie2 = Film(tytul='Cats', rok_wydania='1995', gatunek='dokumentalny')
+movie3 = Film(tytul='Tiger', rok_wydania='1998', gatunek='biograficzny')
+movie4 = Film(tytul='Home', rok_wydania='2005', gatunek='horror')
+
+series1 = Serial(tytul='The Simpsons', rok_wydania='1994', gatunek='komedia', numer_odcinka= 1, numer_sezonu= 1)
+series2 = Serial(tytul='Game of Thrones', rok_wydania='2000', gatunek='obyczajowy', numer_odcinka= 1, numer_sezonu= 1)
+series3 = Serial(tytul='Friends', rok_wydania='1995', gatunek='komedia', numer_odcinka= 1, numer_sezonu= 1)
+series4 = Serial(tytul='Star Trek', rok_wydania='2004', gatunek='horror', numer_odcinka= 1, numer_sezonu= 1)
+
+movie2.play()
+movie2.play()
+movie2.play()
+movie2.play()
+print(movie2.liczba_odtworzen)
+
 print(series2)
 print(series3)
 
@@ -60,21 +77,8 @@ lista_all.append(series3)
 lista_all.append(series4)
 print(lista_all)
 
-def get_movies():
-    movies = []
-    for i in lista_all:
-        if type(i) == film:
-            movies.append(i)
-            sorted_movies = sorted(movies, key=lambda movie: movie.tytul)
-    return sorted_movies
+print(get_movies(lista_all))
+print(get_series(lista_all))
 
-def get_series():
-    series = []
-    for i in lista_all:
-        if type(i) == serial:
-            series.append(i)
-            sorted_series = sorted(series, key=lambda series1: series1.tytul)
-    return sorted_series
-
-print(get_movies())
-print(get_series())
+print(search('Dogs'))
+print(search('Friends'))
