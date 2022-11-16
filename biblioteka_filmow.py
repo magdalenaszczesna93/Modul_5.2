ca313b261d24333e214811f2693905a7eeff732c
Magdalena Szczesna <magdalena.szczesna.ue@gmil.com>
2022-11-16 14:53:49 +0100

    add metode na liczbe odtorzen

--- biblioteka_filmow.py
+++ biblioteka_filmow.py
@@ -3,9 +3,14 @@ class filmy:
         self.tytul = tytul
         self.rok_wydania = rok_wydania
         self.gatunek = gatunek
-        self.liczba_odtwoezen = liczba_odtworzen
+        self.liczba_odtworzen = liczba_odtworzen
     def __str__ (self):
-        return f'{self.tytul} {self.rok_wydania} {self.gatunek} {self.liczba_odtwoezen}'
+        return f'{self.tytul} ({self.rok_wydania})'
+
+#jak zorbić żeby to zwięskzało na stałę listę odtworzeń i za każdym razem dodawało?
+def play (self):
+        self.liczba_odtworzen += 1
+        return f'{self.tytul} odtworzono {self.liczba_odtworzen} razy.'
 
 movie1 = filmy(tytul='Dogs', rok_wydania='1994', gatunek='komedia', liczba_odtworzen=20)
 movie2 = filmy(tytul='Cats', rok_wydania='1995', gatunek='dokumentalny', liczba_odtworzen=10)
@@ -18,12 +23,25 @@ class seriale(filmy):
         self.numer_odcinka = numer_odcinka
         self.numer_sezonu = numer_sezonu
     def __str__ (self):
-        return f'{self.tytul} {self.rok_wydania} {self.gatunek} {self.liczba_odtwoezen} S{self.numer_odcinka:02d}E{self.numer_sezonu:02d}'
+        return f'{self.tytul} {self.rok_wydania} {self.gatunek} {self.liczba_odtworzen} S{self.numer_odcinka:02d}E{self.numer_sezonu:02d}'
+
+def play (self):
+        self.liczba_odtworzen += 1
+        return f'{self.tytul} odtworzono {self.liczba_odtworzen} razy.'
 
 series1 = seriale(tytul='The Simpsons', rok_wydania='1994', gatunek='komedia', liczba_odtworzen=20, numer_odcinka= 1, numer_sezonu= 1)
 series2 = seriale(tytul='Game of Thrones', rok_wydania='2000', gatunek='obyczajowy', liczba_odtworzen=45, numer_odcinka= 1, numer_sezonu= 1)
 series3 = seriale(tytul='Friends', rok_wydania='1995', gatunek='komedia', liczba_odtworzen=35, numer_odcinka= 1, numer_sezonu= 1)
 series4 = seriale(tytul='Star Trek', rok_wydania='2004', gatunek='horror', liczba_odtworzen=10, numer_odcinka= 1, numer_sezonu= 1)
 
-print(movie2)
+lista_all = []
+
+print(play(movie1))
+print(movie1)
+print(play(movie1))
+print(movie1)
+print(play(series2))
+print(series2)
+print(play(series2))
+print(series2)
 print(series3)
